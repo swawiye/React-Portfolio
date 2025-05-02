d7152ec34e42199f6e72bf26e71bbf3be221fa79
swawiye <sovereign.wawiye@gmail.com>
2025-05-02 09:43:20 +0300

    navbar linking using react router

--- src/contents/Navbar.jsx
+++ src/contents/Navbar.jsx
@@ -1,17 +1,20 @@
+//import { useImperativeHandle } from "react";
+import {Link, useMatch, useResolvedPath} from "react-router-dom";
 function Navbar() {
     return <nav className="nav">
-        <a href="/" className="site-title">MyPorfolio</a>
+        <Link to="/" className="site-title">MyPorfolio</Link>
         <ul>
-            <CustomLink href="/AboutMe">About Me</CustomLink>
-            <CustomLink href="/ContactMe">Contact Me</CustomLink>
+            <CustomLink to="/AboutMe">About Me</CustomLink>
+            <CustomLink to="/ContactMe">Contact Me</CustomLink>
         </ul>
     </nav>
 }
 export default  Navbar;
 
-function CustomLink(href, children, ...props) {
-    const path = window.location.pathname
+function CustomLink(to, children, ...props) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
     return(
-        <li className={path === href ? "active" : ""}><a href={href} {...props}>{children}</a></li>
+        <li className={isActive ? "active" : ""}><Link href={to} {...props}>{children}</Link></li>
     )
 }
