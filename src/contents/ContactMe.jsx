793fb72fdae0e5ba9f59ed35418af715f0087595
swawiye <sovereign.wawiye@gmail.com>
2025-05-04 20:25:49 +0300

    form validation

--- src/contents/ContactMe.jsx
+++ src/contents/ContactMe.jsx
@@ -7,10 +7,23 @@
 //     }
 // }
 // export default ReachOut;
+import { useState } from "react";
 
 const ReachOut = () => {
+    const [text, setText] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {}
     return (
         <div>
+            <form className="contact-form">
+                <h3>Reach out to me</h3>
+                <label className="form-label">Name</label>
+                <input className="form-input"></input>
+                <label className="form-label">Message</label>
+                <input className="form-input"></input>
+                <button>Send</button>
+            </form>
             <h3>Phone: +254701374708</h3>
             <h3>Email address: sovereign.wawiye@gmail.com</h3>
             <h3>LinkedIn: Sovereign Wawiye</h3>
